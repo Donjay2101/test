dfcc52a9a941cffdeb203689d2b2adec6c2cf076
Deepak Singh Mehta (DD-DEL/PET-ICT) <deepak.singhmehta@PETRONAS.PETRONET.DIR>
2018-09-18 23:27:56 +0800

    Initial Release

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var addition = require('./addition.js');
+var app = express();
+
+app.set('view engine','jade');
+
+app.get('/',function(req,res){
+
+});
+
+var server = app.listen(3000,function(){
+    console.log(addition.AddNumber(2,5));
+});
+
+
